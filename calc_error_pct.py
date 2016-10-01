c76bcd77441f69f260e7ccead65617d18d83cfe5
Maxim Dunavicher <maxim.d@velismedia.com>
2016-10-01 16:05:20 +0300

    this is my change for misha

--- calc_error_pct.py
+++ calc_error_pct.py
@@ -1,20 +1,3 @@
-__author__ = 'stas'
+"this is my change"
 
-
-def calculate_error_percentage(y_vector, predicted_y_vector):
-    """Gets a vector y of results, and vector y_prediction of predictions, compares, and calculates error percentage
-    """
-
-    # Make sure both vectors are of the same size
-    assert(len(y_vector) == len(predicted_y_vector))
-
-    # Calculate how many errors occurred
-    error_count = 0
-    total_samples = len(y_vector)
-    for i in xrange(total_samples):
-        if y_vector[i] != predicted_y_vector[i]:
-            error_count += 1
-
-    # Return error percentage
-    return (float(error_count) / total_samples) * 100
 
